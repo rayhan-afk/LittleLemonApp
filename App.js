9bfbfc8cb424f46a46809dc048f210f478586747
Rayhan Abduhuda <rayrayhan920@gmail.com>
2024-01-08 13:47:37 +0700

    selesai untuk tanggal 8

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
-
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './screens/WelcomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
@@ -17,22 +14,10 @@ export default function App() {
       <NavigationContainer>
         <View style={styles.container}>
           <LittleLemonHeader />
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({size}) => {
-                let iconName;
-                if (route.name === 'Welcome') {
-                  iconName = 'ios-home';
-                } else if (route.name === 'Login') {
-                  iconName = 'ios-enter';
-                }
-                return <Ionicons name={iconName} size={size} />;
-              },
-            })}
-            initialRouteName="Login">
-            <Tab.Screen name="Welcome" component={WelcomeScreen} />
-            <Tab.Screen name="Login" component={LoginScreen} />
-          </Tab.Navigator>
+          <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+          </Drawer.Navigator>
         </View>
         <View style={styles.footerContainer}>
           <LittleLemonFooter />
